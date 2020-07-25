269f53334aa9d27f8ff14aeed711d5fd4eb2fe86
Abijah Kabiro <abijahkabiro@gmail.com>
2020-07-25 19:17:22 +0300

    add an alert that tells the user thank you after inputting name,email,message.

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,17 @@ $(document).ready(function() {
       function(){
         $(this).animate({opacity:'0'});
       });
+
+      $('.button').click(function () {
+        var Name = $('#name').val();
+        var Email = $('#email').val();
+        var Message = $('#message');
+        if (Name == '' || Email == '' || Message == '') {
+            alert('Please make sure you have filled in the form correctly!');
+        } else {
+            alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+        }
+  });
     
       
 });
